64803e94395942d4ae01eb07e6ca742082e3df7b
minji <zzz0525z@naver.com>
2025-06-23 17:44:42 +0900

    스크립트 태그 수정

--- resources/views/layouts/popup.blade.php
+++ resources/views/layouts/popup.blade.php
@@ -26,5 +26,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    @stack('scripts')
 </body>
 </html>
