ec538b9def93d4025f36e032afd0a72b04492d35
minji <zzz0525z@naver.com>
2025-06-27 14:09:29 +0900

    권한에 따른 url 수정

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -1,24 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.popup')
 
 @section('content')
-<div class="mt-5">
-    <!-- <div class="with mb-4">
-        <h1 class="text-2xl font-bold">쪽지 상세보기</h1>
-        <a href="{{ url('/') }}">
-            <img src="/img/home.png" width="30px" style="align-content: center;float:right;">
-        </a>
-    </div> -->
+<div class="container mt-5">
+    <div class="with mb-4">
+        <h3 class="text-2xl font-bold">쪽지 상세보기</h3>
+    </div>
 
     <div class="card mb-4">
         <div class="card-body">
             <h4 class="card-title">{{ $message->title }}</h4>
             <h6 class="card-subtitle mb-2 text-muted">
-                보낸이 : {{ $message->sender_id }} |
+                발신자 : {{ $message->name }} |
                 시간 : {{ $message->created_at->format('Y-m-d H:i') }}
             </h6>
-            <p class="card-text mt-5">{{ $message->content }}</p>
+            <p class="card-text mt-5">{!! $message->content !!}</p>
             <div class="mt-5">
-                <a href="{{ route('boards.show', $message->post_no) }}" target="_blank" class="btn btn-primary">원글 이동</a>
+                @auth
+                    @php
+                        if ( isset($message->status) && $message->status && auth()->user()->is_admin === 'Y' ){
+                            $url = route('boards.index', ['div' => 'O']);
+                            $comment = '리스트 확인';
+                        }
+                        else {
+                            $url = route('request.show', ['id' => $message->post_no]);
+                            $comment = '원글 이동';
+                        }
+                    @endphp
+                @endauth
+                <a href="{{ $url }}" target="_blank" class="btn btn-primary">{{ $comment }}</a>
             </div>
         </div>
     </div>
