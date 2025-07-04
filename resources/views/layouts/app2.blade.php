8087c1b62f98f676f85c17f2343ee486d29767ad
minji <zzz0525z@naver.com>
2025-07-04 17:09:36 +0900

    js 수정

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -66,10 +66,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js"></script>
-    <!-- <script src="{{ asset('js/app.js') }}"></script> -->
-    @vite([
-        'resources/js/app.js'
-    ])
+    <script src="{{ asset('js/app.js') }}"></script>
     
     {{-- 팝업(primary) --}}
     @if(session('msg'))
