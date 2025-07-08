3a7641ca332921a8f048812ffba4aaff3aa2975f
minji <zzz0525z@naver.com>
2025-07-08 11:15:28 +0900

    문구 수정

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2025</p></div>
+    <div class="container"><p class="m-0 text-center text-white">© 2025 mjnadev. All rights reserved.</p></div>
 </footer>
