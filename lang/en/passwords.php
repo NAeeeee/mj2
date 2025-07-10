53419b2070b840ad7a90a68a42fdff4a511885b8
minji <zzz0525z@naver.com>
2025-07-10 14:11:44 +0900

    lang 추가

---
+++ lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '비밀번호 재설정이 완료되었습니다.',
+    'sent' => '비밀번호 재설정 링크를 이메일로 성공적으로 보냈습니다.',
+    'throttled' => '잠시만 기다려주세요. 비밀번호 재설정 요청이 너무 자주 발생했습니다.',
+    'token' => '유효한 토큰이 아닙니다.',
+    'user' => "해당 이메일을 찾을 수 없습니다. 이메일 주소를 확인해주세요.",
+
+];
