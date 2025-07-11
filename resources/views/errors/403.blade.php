fc42e0d537b08dc1c4eed4358608651983c1ee49
minji <zzz0525z@naver.com>
2025-07-11 15:40:33 +0900

    수정

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen px-4">
     <div class="text-center max-w-md">
-        <img src="/img/ongi.jpg" class="mx-auto w-40 h-40 object-cover rounded-full shadow-lg mb-4" alt="옹이">
+        <img src="/img/ongi.jpg" class="mx-auto w-40 h-40 object-cover rounded-full shadow-lg mb-4">
         <h1 class="text-6xl font-extrabold text-gray-700 mb-2">403</h1>
         <p class="text-xl text-gray-800 mb-2">페이지를 확인해주세요.</p>
         <p class="text-sm text-gray-500 mb-6">권한이 없거나 접근이 차단된 페이지에요.</p>
